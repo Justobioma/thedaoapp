1cc41fe31b56c63909d5215259fac51fb92465ca
Obioma <justobioma@outlook.com>
2022-05-19 18:13:00 +0100

    commit all files to repo

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x13F279a3abD33D1b463DB8A1B8279844f04Ea883";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
